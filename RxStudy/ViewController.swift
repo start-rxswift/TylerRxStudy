a647a4809d4ac9bbee0a6f4ddb7410938d93578f
kimtaesu <kimtaesoo188@gmail.com>
2019-08-07 23:26:22 +0900

    subscribeOn 설명

--- RxStudy/ViewController.swift
+++ RxStudy/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
+    @IBOutlet weak var subscribeOnButton: UIButton!
+    @IBOutlet weak var observableSubscribeOn: UIButton!
+    @IBOutlet weak var buttonSubscribeOn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        subscribeOnButton.rx.tap
+            .subscribeOn(SerialDispatchQueueScheduler(internalSerialQueueName: "test"))
+            .subscribe(onNext: {
+                print("subscribeOnButton ThreadName: \(threadName())")
+            })
 
 
+        buttonSubscribeOn.rx.tap
+            .concatMap { self.createObservable("concatMap") }
+            .do(onNext: { _ in
+                print("concatMap ThreadName: \(threadName())")
+            })
+            .flatMap { _ in self.createObservable("flatMap") }
+            .do(onNext: { _ in
+                print("flatMap ThreadName: \(threadName())")
+            })
+            .subscribe(onNext: { _ in
+                print("subscribe ThreadName: \(threadName())")
+            })
+    }
+    @IBAction func tapObservableSubscribeOn(_ sender: Any) {
+        Observable.just(1)
+            .concatMap { _ in self.createObservable("concatMap") }
+            .do(onNext: { _ in
+                print("concatMap ThreadName: \(threadName())")
+            })
+            .flatMap { _ in self.createObservable("flatMap") }
+            .do(onNext: { _ in
+                print("flatMap ThreadName: \(threadName())")
+            })
+            .subscribe(onNext: { _ in
+                print("tapObservableSubscribeOn ThreadName: \(threadName())")
+            })
+    }
+    func createObservable(_ with: String) -> Observable<Int> {
+        return Observable.just(1).subscribeOn(SerialDispatchQueueScheduler(internalSerialQueueName: with))
+    }
 }
 
